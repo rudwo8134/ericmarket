adfbdf95057ff8c6a1b56fa3432ff3f78d934c6c
Eric Shin <92757570+eshin-at-speer@users.noreply.github.com>
2022-01-31 22:51:33 -0500

    add the data and image on body

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-6. List Products
+
+## Day1. List Products
   1. add data.js
   2. add Images
   3. render products
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,47 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@material-ui/core';
+import Head from 'next/head';
+import Image from 'next/image';
 import Layout from '../components/Layout';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import data from '../utils/data';
 
 export default function Home() {
   return (
     <Layout>
       <div>
         <h1>products</h1>
-        <ul>
-          <li>Product1</li>
-          <li>Product2</li>
-          <li>Product3</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data?.products?.map((product) => (
+            <Grid item md={4} key={product?.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product?.image}
+                    title={product?.name}
+                  />
+                  <CardContent>
+                    <Typography>{product?.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${product?.price}</Typography>
+                  <Button size="small" color="primary">Add to Cart</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
-    
     </Layout>
   );
 }
